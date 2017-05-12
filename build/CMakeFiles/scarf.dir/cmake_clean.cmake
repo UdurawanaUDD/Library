d42eb8df05c066408586c19e010d3d5c5db44f29
Pahan Silva <pahan.silva@ifsworld.com>
2017-05-12 12:07:59 +0530

    Remove build directory

--- build/CMakeFiles/scarf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libscarf.pdb"
-  "libscarf.so"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/scarf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
